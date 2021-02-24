cf5b2178de89b929bca34c0a1c8ba66089759067
Jesse Willig <jessewillig@outlook.com>
2021-02-23 18:00:28 -0800

    fixed missing []

--- develop/utilities/generatemarkdown.js
+++ develop/utilities/generatemarkdown.js
@@ -1,7 +1,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge (license) {
   if (license !== "None") {
-    return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    return `[![License](https://img.shields.io/badge/license-${license}-blue.svg)]`;
   }; 
     return " ";
 };
